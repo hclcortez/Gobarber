9f79c58f91572a984bf33e239e81ea43c7908e4e
Hover Cortez leite <hclcortez@gmail.com>
2019-07-14 09:16:38 -0300

    MOstrando as imagens do usuários

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const upload = require('multer')(multerConfig)
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
+const FileController = require('./app/controllers/FileController')
 
 const authMiddleware = require('./app/middlewares/auth')
 const guestMiddleware = require('./app/middlewares/guest')
@@ -17,6 +18,8 @@ routes.use((req, res, next) => {
 
   return next()
 })
+
+routes.get('/files/:file', FileController.show)
 
 routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
